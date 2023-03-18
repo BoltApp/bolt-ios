720ff22af3b267091f8cbb2a2547ee937bde60be
Mehul Dhorda <mdhorda@bolt.com>
2023-03-18 15:18:54 -0400

    Initial package upload

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+  name: "Bolt",
+  platforms: [.iOS(.v13)],
+  products: [
+    .library(
+      name: "Bolt",
+      targets: ["Bolt"]
+    )
+  ],
+  dependencies: [],
+  targets: [
+    .binaryTarget(
+      name: "Bolt",
+      url: "https://bolt-mobile-interview.s3.us-west-1.amazonaws.com/Bolt.xcframework.zip",
+      checksum: "bde03998f4ff2afedc31bf036fd9178a113c6dedd759a2b024c0f7f7e1c859e4"
+    )
+  ]
+)
